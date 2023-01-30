50bc2e1186f1c6adb8c97194c268fadce6331881
Ivan <romankelmakov19@gmail.com>
2023-01-30 21:48:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// var number = 1;
+// var string = "Green";
+// var sym = Symbol();
+// var boolean = true;
+// let person;
+// person = {
+//   name: "Someone",
+//   age: 25,
+//   isMarried: true,
+// };
+
+// let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
+// console.log(arr[0]);
+
+// let answer = prompt("are you 18?");
+
+// console.log(typeof null);
+
+// console.log("arr" + "object");
+// console.log("arr" + "object");
+
+a = 4;
+
+console.log(a);
+
+let isChecked = true;
+let isClose = false;
+
+console.log(isChecked && !isClose);
